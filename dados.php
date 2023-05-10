a619f4bb1abe754f369e626aca290b48e1673746
Lucas Rodrigues PC <lucasrodrigues2606@gmail.com>
2023-05-10 20:23:35 -0300

    pronto

--- dados.php
+++ dados.php
@@ -1,8 +1,10 @@
 <?php
-$dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
-if (empty($dados['nome']) || empty($dados['email']) || empty($dados['cpf']) || empty($dados['genero'])) {
-    $retorna = ['status' => false, 'msg' => "Erro! Preencha todos os campos obrigatórios!"];
+
+$dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);                                                        // Recebe os dados doformulário via POST e armazena em um array associativo
+
+if (empty($dados['nome']) || empty($dados['email']) || empty($dados['cpf']) || empty($dados['genero'])) {       // Verifica se as chaves do array estão vazias ou não foram definidas.
+    $retorna = ['status' => false, 'msg' => "Preencha todos os campos obrigatórios!"];
 
 } elseif (!filter_var($dados['email'], FILTER_VALIDATE_EMAIL)) {
     $retorna = ['status' => false, 'msg' => "Erro! Insira um email válido!"];
@@ -20,35 +22,14 @@ if (empty($dados['nome']) || empty($dados['email']) || empty($dados['cpf']) || e
                 '-------------------------------------'."\n";
 	// Salva a string no arquivo de texto
     file_put_contents('dados.txt', $conteudo, FILE_APPEND | LOCK_EX);
-    $retorna = ['status' => true, 'msg' => "Sucesso"];
+    $retorna = ['status' => true, 'msg' => "SUCESSO!"];
 	
 }
 
-echo json_encode($retorna); 
+// Retorna a resposta em formato JSON
+echo json_encode($retorna);
 
-/*// Recebe os dados do formulário
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$cpf = $_POST['cpf'];
-$genero = $_POST['genero'];
+?>
 
-// Valida os dados do formulário
-if (empty($nome) || empty($email) || empty($cpf) || empty($genero)) {
-    echo 'Por favor, preencha todos os campos.';
 
-} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-    echo 'Por favor, informe um endereço de e-mail válido.';
 
-} elseif (!preg_match('/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/', $cpf)) {
-    echo 'Por favor, informe um CPF válido.';
-
-} else {
-    // Cria uma nova string com os dados do formulário
-    $newData = "Nome: $nome\nEmail: $email\nCPF: $cpf\nGênero: $genero\n\n";
-
-    // Salva a string no arquivo de texto
-    file_put_contents('data.txt', $newData, FILE_APPEND);
-
-    echo 'Dados enviados com sucesso!';
-}
-*/
--- js/backend.js
+++ js/backend.js
@@ -4,29 +4,29 @@
 
 const form = document.getElementById("form");
 
-if (form) {
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-
-    const dadosForm = new FormData(form);
-
-    const dados = await fetch("dados.php", {
+if (form) {                                                                     // Verifica se o elemento 'form' existe
+  form.addEventListener("submit", async (e) => {                                // Adiciona um ouvinte de evento ao formulário
+    e.preventDefault();                                                         // Evita que a página seja recarregada ou redirecionada quando o formulário é enviado
+                                                                                
+    const dadosForm = new FormData(form);                                       // Cria um objeto FormData que coleta os dados do formulário
+                                                                                
+    const dados = await fetch("dados.php", {                                    // Faz uma requisição usando o método POST e envia os dados do formulário para 'dados.php'
       method: "POST",
       body: dadosForm,
     });
 
-    const resposta = await dados.json();
+    const resposta = await dados.json();                                        // Converte a resposta da requisição em JSON
     console.log(resposta);
 
-    if (resposta["status"]) {
+    if (resposta["status"]) {                                                   // Se status da resposta for true segue se não vai pro else
       const alerta = document.createElement("div");
       alerta.classList.add("alerta-sucesso");
-      alerta.innerText = resposta["msg"];
-      document.getElementById("alerta-sucesso").appendChild(alerta);
+      alerta.innerText = resposta["msg"];                                       // Cria a div da classe 'alerta-sucesso' e atribui o valor da propriedade 'msg'
+      document.getElementById("alerta-sucesso").appendChild(alerta);            // Conecta 'alerta' ao elemento com o ID 'alerta-sucesso'
 
       setTimeout(() => {
         alerta.remove();
-      }, 2500);
+      }, 3500);
       
     } else {
       const alerta = document.createElement("div");
@@ -36,11 +36,10 @@ if (form) {
 
       setTimeout(() => {
         alerta.remove();
-      }, 2500);
+      }, 3500);
     }
   });
 }
-
 
 
 /*const form = document.getElementById("form");
