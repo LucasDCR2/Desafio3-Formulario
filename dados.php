ff90189e2f2690ba85746c64d49fe77791b42145
Lucas Rodrigues PC <lucasrodrigues2606@gmail.com>
2023-05-10 22:55:52 -0300

    pronto

--- dados.php
+++ dados.php
@@ -1,17 +1,18 @@
 <?php
-
+require 'vendor/autoload.php';                                                                                  // Incluindo o arquivo da biblioteca
+use Respect\Validation\Validator as v;
 
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);                                                        // Recebe os dados doformulário via POST e armazena em um array associativo
 
 if (empty($dados['nome']) || empty($dados['email']) || empty($dados['cpf']) || empty($dados['genero'])) {       // Verifica se as chaves do array estão vazias ou não foram definidas.
     $retorna = ['status' => false, 'msg' => "Preencha todos os campos obrigatórios!"];
 
-} elseif (!filter_var($dados['email'], FILTER_VALIDATE_EMAIL)) {
-    $retorna = ['status' => false, 'msg' => "Erro! Insira um email válido!"];
+} elseif (!filter_var($dados['email'], FILTER_VALIDATE_EMAIL)) {                                                // Verifica se o CPF está no formato correto usando o preg_match()
+    $retorna = ['status' => false, 'msg' => "Erro! Insira um email válido!"];                                   //Se o formato estiver correto, o código então verifica se o CPF é válido usando a biblioteca
 
-} elseif (!preg_match("/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/", $dados['cpf'])) {
+} else if (!preg_match("/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/", $dados['cpf']) || !v::cpf()->validate(str_replace(['.', '-'], '', $dados['cpf']))) {
     $retorna = ['status' => false, 'msg' => "Erro! Insira um CPF válido!"];
-	
+
 } else {
 	// Cria uma nova string com os dados do formulário
     $conteudo = '-------------------------------------'."\n".
